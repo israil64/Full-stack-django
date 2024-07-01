df1fceaee9c1ca5976f21a76e896ee01981134e9
israil64 <md3096720@gmail.com>
2024-07-01 13:14:41 +0530

    done setting

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 
 echo "BUILD START"
-python3.9 -m pip install -r requirements.text
+source "C:\Users\DELL\OneDrive\Desktop\Django Fullstack\.venv\Scripts\activate"
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
