a76e0fea0f54bda7efea60c7eb8d03838e630e05
israil64 <md3096720@gmail.com>
2024-07-11 23:42:28 +0530

    Update build_files.sh to activate virtual environment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
-# build_files.sh
+# Activate the virtual environment
+.venv\Scripts\activate
+
+
+# Install dependencies
 pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+# Run database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
